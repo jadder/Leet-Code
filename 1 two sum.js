68394c04d1fc3798f7c5a8d5ac571f719152f761
DESKTOP-HEOGP8O\Jadder <jadder.moya@gmail.com>
2024-07-02 20:46:38 -0400

    Comments updated

--- 1 two sum.js
+++ 1 two sum.js
@@ -1,3 +1,17 @@
+/*
+Solution:
+  Given that there always exists a pair a + b = target, regardless of whether 
+  the target number is negative or positive, the solution remains the same.
+  
+  Steps to find the pair [X, Y] where X + Y = target:
+  
+  1. Take a number X from the array and subtract it from the target to find the 
+     second number Y needed.
+  2. Find the index of X from the beginning (START) to the end (END) of the array.
+  3. Find the index of Y from the end (END) to the beginning (START) of the array.
+  4. Return [X, Y].
+*/
+
 var twoSum = function (nums, target) {
   let a = 0,
     b = 0;
